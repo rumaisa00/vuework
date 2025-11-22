10b1b25e10d762f835e028f3ad1879842ce981dd
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-22 10:06:49 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
+// Create Vue app
 const app = createApp(App)
+
+// Create Pinia store
 const pinia = createPinia()
 
+// Use Pinia and Router
 app.use(pinia)
 app.use(router)
 
+// Mount app
 app.mount('#app')
