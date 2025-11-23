af4fb8d5f61ab5dcf586ca7307db89fed54e1fd6
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-24 01:42:20 +0300

    Update OrdersView.vue

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -23,7 +23,7 @@
 
 <script setup>
 import { computed } from 'vue'
-import { useUserStore } from '../stores/user'
+import { useUserStore } from '../stores/user.js'  <!-- Correct path with extension -->
 
 const user = useUserStore()
 const orders = computed(() => user.user?.orders || [])
