ba0c0cea5613e10a891a02ab34e718024690b828
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-23 23:54:54 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,55 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 // Views
-import HomeView from '../views/HomeView.vue'
-import AdminView from '../views/AdminView.vue'
-import EmployeeView from '../views/EmployeeView.vue'
-import CustomerView from '../views/CustomerView.vue'
 import LoginView from '../views/LoginView.vue'
 import SignupView from '../views/SignupView.vue'
-import PetsView from '../views/PetsView.vue'
-import FoodView from '../views/FoodView.vue'
-import SuppliesView from '../views/SuppliesView.vue'
-import OrdersView from '../views/OrdersView.vue'
-import CartView from '../views/CartView.vue'
-import BillView from '../views/BillView.vue'
-import NotFoundView from '../views/NotFoundView.vue'
+import CustomerView from '../views/CustomerView.vue'
+import AdminView from '../views/AdminView.vue'
+import Cart from '../views/Cart.vue'
+import Order from '../views/Order.vue'
+import Bill from '../views/Bill.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: LoginView
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: SignupView
+  },
+  {
+    path: '/customer',
+    name: 'Customer',
+    component: CustomerView
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: AdminView
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/orders',
+    name: 'Order',
+    component: Order
+  },
+  {
+    path: '/bill',
+    name: 'Bill',
+    component: Bill
+  }
+]
 
 const router = createRouter({
-  history: createWebHashHistory(),   // IMPORTANT for GitHub Pages
-  routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/admin', name: 'admin', component: AdminView },
-    { path: '/employee', name: 'employee', component: EmployeeView },
-    { path: '/customer', name: 'customer', component: CustomerView },
-    { path: '/login', name: 'login', component: LoginView },
-    { path: '/signup', name: 'signup', component: SignupView },
-    { path: '/manage-pets', name: 'manage-pets', component: PetsView },
-    { path: '/manage-food', name: 'manage-food', component: FoodView },
-    { path: '/manage-supplies', name: 'manage-supplies', component: SuppliesView },
-    { path: '/orders', name: 'orders', component: OrdersView },
-    { path: '/cart', name: 'cart', component: CartView },
-    { path: '/bill', name: 'bill', component: BillView },
-
-    // Custom 404
-    { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundView }
-  ]
-})
-
-// Title updater
-router.beforeEach((to, from, next) => {
-  document.title = to.meta?.title || 'SmartPetShop'
-  next()
+  history: createWebHistory(),
+  routes
 })
 
 export default router
