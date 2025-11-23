d5ee6fccbcd2a1112016d1e94ccb4188c3bf02e7
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-24 01:08:10 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script setup>
-import Header from './components/AppHeader.vue'
-import Footer from './components/AppFooter.vue'
+import Header from './components/layout/AppHeader.vue'
+import Footer from './components/layout/AppFooter.vue'
 </script>
 
 <style>
