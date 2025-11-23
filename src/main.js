ea5662f4d1b5f0945ae67dd0301b8a6cf3cd7ec8
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-23 23:53:45 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
-import './assets/main.css' // keep this as your global modern css
+import './assets/main.css'
 
 const app = createApp(App)
 
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
-
-app.use(pinia)
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
