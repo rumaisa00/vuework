953d577c3909d18d15cfdd287138cafc0d37639c
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-24 01:27:45 +0300

    Update CustomerView.vue

--- src/views/CustomerView.vue
+++ src/views/CustomerView.vue
@@ -29,7 +29,7 @@
 import { ref, computed } from "vue";
 import { useProductStore } from "../views/productView.vue";
 import { useCartStore } from "../stores/cart.js";
-import Header from "../components/AppHeader.vue";
+import Header from "../components/layout/AppHeader.vue";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
