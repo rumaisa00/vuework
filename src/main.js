300eb5495296095dc2735372a51fa04125cfc32b
Alaa malik <ensw00822@student.su.edu.krd>
2025-11-23 21:42:17 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import { createApp } from 'vue' 
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
-import './assets/main.css'
+import './assets/main.css' // if you have global styles
 
 const app = createApp(App)
+
+// Initialize Pinia with persistence plugin
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
