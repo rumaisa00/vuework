63b06c89f3ac064d27d35fa34c205b79661b39d7
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-24 01:54:42 +0300

    Update SignupView.vue

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -20,7 +20,7 @@
 
 <script setup>
 import { ref } from "vue";
-import { useUserStore } from "../stores/user.js"; // Add .js extension
+import { useUserStore } from "../stores/user.js"; 
 import { useRouter } from "vue-router";
 
 const router = useRouter();
