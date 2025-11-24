f2b5ca60c94620386b89f8dcc44fa1cbaef2e3ff
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-24 03:59:26 +0300

    Update SignupView.vue

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,3 +1,13 @@
+<template>
+  <div class="signup-container">
+    <h1>Signup</h1>
+    <input v-model="username" placeholder="Username" />
+    <input v-model="password" placeholder="Password" type="password" />
+    <button @click="signupUser">Signup</button>
+    <router-link to="/">Login</router-link>
+  </div>
+</template>
+
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
@@ -9,22 +19,15 @@ const userStore = useUserStore()
 const username = ref('')
 const password = ref('')
 
-function signup() {
+function signupUser() {
   if (!username.value || !password.value) {
     alert('Enter username and password')
     return
   }
 
-  // Fake signup (you can add to users array in user store)
-  const newUser = {
-    id: Date.now(),
-    name: username.value,
-    role: 'customer'
-  }
-
-  userStore.addUser?.(newUser)  // if addUser exists
-  userStore.login(newUser)      // log in immediately
-
+  const newUser = { id: Date.now(), name: username.value, role: 'customer', orders: [] }
+  userStore.addUser(newUser)
+  userStore.login(newUser)
   router.push({ name: 'Customer' })
 }
 </script>
