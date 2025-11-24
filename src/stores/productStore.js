b5160c852930c8c6df2c3dc24168a9f2fc396717
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-24 03:51:05 +0300

    Update productStore.js

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -1,32 +1,29 @@
-// src/stores/productStore.js
 import { defineStore } from 'pinia'
 
 export const useProductsStore = defineStore('products', {
   state: () => ({
-    pets: [],
-    food: [],
-    supplies: [],
+    pets: [
+      { id: 1, name: 'Dog', price: 100, stock: 5, image: 'https://place-puppy.com/200x200' },
+      { id: 2, name: 'Cat', price: 80, stock: 3, image: 'https://placekitten.com/200/200' }
+    ],
+    food: [
+      { id: 1, name: 'Dog Food', price: 20, stock: 10, image: 'https://placekitten.com/200/200' },
+      { id: 2, name: 'Cat Food', price: 15, stock: 8, image: 'https://placekitten.com/200/201' }
+    ],
+    supplies: [
+      { id: 1, name: 'Collar', price: 10, stock: 15, image: 'https://placekitten.com/200/202' }
+    ]
   }),
+  getters: {
+    allProducts: state => [
+      ...state.pets.map(p => ({ ...p, type: 'pets' })),
+      ...state.food.map(f => ({ ...f, type: 'food' })),
+      ...state.supplies.map(s => ({ ...s, type: 'supplies' }))
+    ]
+  },
   actions: {
-    addPet(pet) { this.pets.push(pet) },
-    addFood(food) { this.food.push(food) },
-    addSupply(supply) { this.supplies.push(supply) },
-
-    updatePet(id, payload) {
-      const idx = this.pets.findIndex(p => p.id === id)
-      if (idx !== -1) this.pets[idx] = { ...this.pets[idx], ...payload }
-    },
-    updateFood(id, payload) {
-      const idx = this.food.findIndex(f => f.id === id)
-      if (idx !== -1) this.food[idx] = { ...this.food[idx], ...payload }
-    },
-    updateSupply(id, payload) {
-      const idx = this.supplies.findIndex(s => s.id === id)
-      if (idx !== -1) this.supplies[idx] = { ...this.supplies[idx], ...payload }
-    },
-
-    removePet(id) { this.pets = this.pets.filter(p => p.id !== id) },
-    removeFood(id) { this.food = this.food.filter(f => f.id !== id) },
-    removeSupply(id) { this.supplies = this.supplies.filter(s => s.id !== id) },
+    addPet(p) { this.pets.push(p) },
+    addFood(f) { this.food.push(f) },
+    addSupply(s) { this.supplies.push(s) }
   }
 })
