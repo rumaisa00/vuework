a94dfae36839fe04129f66189299114b223ee09f
rumaisa00 <rumaisa01031216@gmail.com>
2025-11-24 04:00:53 +0300

    Update productStore.js

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -3,27 +3,25 @@ import { defineStore } from 'pinia'
 export const useProductsStore = defineStore('products', {
   state: () => ({
     pets: [
-      { id: 1, name: 'Dog', price: 100, stock: 5, image: 'https://place-puppy.com/200x200' },
-      { id: 2, name: 'Cat', price: 80, stock: 3, image: 'https://placekitten.com/200/200' }
+      { id: 1, name: 'Dog', price: 100, stock: 5, image: 'https://place-puppy.com/200x200' }
     ],
     food: [
-      { id: 1, name: 'Dog Food', price: 20, stock: 10, image: 'https://placekitten.com/200/200' },
-      { id: 2, name: 'Cat Food', price: 15, stock: 8, image: 'https://placekitten.com/200/201' }
+      { id: 1, name: 'Dog Food', price: 20, stock: 10, image: 'https://placekitten.com/200/200' }
     ],
     supplies: [
-      { id: 1, name: 'Collar', price: 10, stock: 15, image: 'https://placekitten.com/200/202' }
+      { id: 1, name: 'Collar', price: 10, stock: 15, image: 'https://placekitten.com/200/201' }
     ]
   }),
   getters: {
-    allProducts: state => [
+    allProducts: (state) => [
       ...state.pets.map(p => ({ ...p, type: 'pets' })),
       ...state.food.map(f => ({ ...f, type: 'food' })),
       ...state.supplies.map(s => ({ ...s, type: 'supplies' }))
     ]
   },
   actions: {
-    addPet(p) { this.pets.push(p) },
-    addFood(f) { this.food.push(f) },
-    addSupply(s) { this.supplies.push(s) }
+    addPet(pet) { this.pets.push(pet) },
+    addFood(food) { this.food.push(food) },
+    addSupply(supply) { this.supplies.push(supply) }
   }
 })
